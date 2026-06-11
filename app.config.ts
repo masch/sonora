aeddfb6f1f711f90243b246d3985f985d0c85b58
masch <the.masch@gmail.com>
2026-06-11 16:44:31 -0300

    revert: remove unnecessary plugins from app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -43,9 +43,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     ],
     'expo-localization',
     'expo-asset',
-    'expo-audio',
-    'expo-image',
-    'expo-sqlite',
     [
       'expo-font',
       {
