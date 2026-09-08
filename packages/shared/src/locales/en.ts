f0e853a14c3751cf4a8cee780f5a60e5b97a2c3b
masch <the.masch@gmail.com>
2026-09-08 17:43:28 -0300

    refactor: remove duplicate instruction keys in English locale file

--- packages/shared/src/locales/en.ts
+++ packages/shared/src/locales/en.ts
@@ -108,7 +108,7 @@ export const en = {
     localMessages: 'Local messages',
     localMessagesSub: 'Community voices',
     instructionsName: 'Before you begin',
-    instructionsSubtitle: 'Instructions to improve your experience'
+    instructionsSubtitle: 'Instructions to improve your experience',
   },
   index: {
     loading: 'Loading…',
